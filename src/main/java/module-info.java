880b365298e58e98b5c2f6b37010e55e99ed2bdf
OsaidB <o.osaidb2015@gmail.com>
2023-07-10 01:18:08 +0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.comp333_con {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.comp333_con to javafx.fxml;
+    exports com.example.comp333_con;
+}
